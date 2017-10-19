a383cecafde9303580efb598712476b6d7817c6b
piusjp <piusjuanp@gmail.com>
2017-10-19 19:29:12 +0700

    Merge origin/master

--- src/main/java/com/Klinik/KlinikApplication.java
+++ src/main/java/com/Klinik/KlinikApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KlinikApplication {
-
+//Main
 	public static void main(String[] args) {
 		SpringApplication.run(KlinikApplication.class, args);
 	}
